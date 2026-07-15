06bfd3b720a3df475b052b9f4c8f9ed4e5cadd33
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-16 01:36:00 +0700

    feat: scaffold EF Core models and DbContext

---
+++ dotnet06_CybersoftMarketPlace.Infrastructure/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnet06_CybersoftMarketPlace.Api;
+
+public partial class Role
+{
+    public int Id { get; set; }
+
+    public string RoleName { get; set; } = null!;
+
+    public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+}
---
+++ dotnet06_CybersoftMarketPlace.Infrastructure/Models/VGetAllProductsDetailV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnet06_CybersoftMarketPlace.Api;
+
+public partial class VGetAllProductsDetailV
+{
+    public int Id { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public int ProductVariantId { get; set; }
+
+    public string? ProductVariantName { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string TenShop { get; set; } = null!;
+}
