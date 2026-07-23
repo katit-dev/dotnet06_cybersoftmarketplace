b738e23543b081615f97b4029504ce8cace43469
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-07-24 05:10:52 +0700

    create RegisterUser

--- dotnet06_CybersoftMarketPlace.Application/Services/UserService.cs
+++ dotnet06_CybersoftMarketPlace.Application/Services/UserService.cs
@@ -1,4 +1,6 @@
 
+using backend_netcore_dotnet06.Helper;
+using Infrastructure.Models;
 using Infrastructure.Repositories;
 
 public interface IUserService
@@ -22,8 +24,49 @@ public class UserService : IUserService
         _userRepository = userRepository;
         _userRoleRepository = userRoleRepository;
     }
-    public Task RegisterUserAsync(UserRegisterDTO model)
+    public async Task RegisterUserAsync(UserRegisterDTO model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            // 1/ insert vào bảng user
+            User userModel = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = model.Username,
+                FullName = model.FullName,
+                Alias = HelperFunction.StringToSlug(model.FullName),
+                Email = model.Email,
+                Phone = model.Phone,
+                Avatar = @$"https://ui-avatars.com/api/?name={model.FullName}&background=random&size=128",
+                PasswordHash = HelperFunction.HashPassword(model.Password),
+                Address = model.Address,
+                CreatedAt = DateTime.Now,
+                Deleted = false,
+            };
+
+            await _userRepository.AddAsync(userModel);
+
+            // 2/ insert vào bảng user_role
+
+            // Add liên bảng userrole
+            UserRole userRoleModel = new UserRole
+            {
+                UserId = userModel.Id,
+                RoleId = UserRoleConst.User, // Mặc định khi đăng ký sẽ là role user
+            };
+
+            userModel.UserRoles.Add(userRoleModel);
+
+            // Sau khi thêm userrole từ usermodel thì SaveChangesAsync
+            // sẽ tự động thêm vào bảng UserRole
+            await _unitOfWork.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            await _unitOfWork.RollbackTransactionAsync();
+        }
+
+
     }
+
 }
