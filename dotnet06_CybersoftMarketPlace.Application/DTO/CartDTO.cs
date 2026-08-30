058ba5fd9a1d24ac854bc193b28aa20f03899b42
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-08-30 13:37:39 +0700

    feat(cart): add CartDTO

---
+++ dotnet06_CybersoftMarketPlace.Application/DTO/CartDTO.cs
@@ -0,0 +1,16 @@
+public class CartDTO
+{
+    public string UserId { get; set; } = Guid.Empty.ToString(); //Id của user đang đăng nhập, để phân biệt giỏ hàng của từng user
+    public DateTime CreatedDate { get; set; } = DateTime.Now; //Ngày tạo giỏ hàng
+    public List<CartItemDTO> CartItems { get; set; } = new List<CartItemDTO>(); //Danh sách các sản phẩm variant trong giỏ hàng
+}
+
+
+public class CartItemDTO
+{
+    public int ProductVariantId { get; set; } //Id của sản phẩm variant
+    public int Quantity { get; set; } //Số lượng sản phẩm variant trong giỏ hàng
+    public decimal Price { get; set; } //Giá của sản phẩm variant
+    public string Image { get; set; } = string.Empty; //Hình ảnh của sản phẩm variant
+    public string Name { get; set; } = string.Empty; //Tên của sản phẩm variant
+}
