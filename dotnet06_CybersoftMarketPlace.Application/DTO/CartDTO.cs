16b8a1b768ed1ed2b5d9045ba3549d4b9bb3672d
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-08-31 00:06:14 +0700

    add AddItemToCartDTO

--- dotnet06_CybersoftMarketPlace.Application/DTO/CartDTO.cs
+++ dotnet06_CybersoftMarketPlace.Application/DTO/CartDTO.cs
@@ -1,6 +1,12 @@
+/*
+using Infrastructure.Models;
+    1 giỏ hàng sẽ có nhiều sản phẩm variant id, quantity, price, image, name
+*/
+
+
 public class CartDTO
 {
-    public string UserId { get; set; }
+    public string UserId { get; set; } = Guid.Empty.ToString(); //Id của user đang đăng nhập, để phân biệt giỏ hàng của từng user
     public DateTime CreatedDate { get; set; } = DateTime.Now; //Ngày tạo giỏ hàng
     public List<CartItemDTO> CartItems { get; set; } = new List<CartItemDTO>(); //Danh sách các sản phẩm variant trong giỏ hàng
 }
@@ -13,4 +19,11 @@ public class CartItemDTO
     public decimal Price { get; set; } //Giá của sản phẩm variant
     public string Image { get; set; } = string.Empty; //Hình ảnh của sản phẩm variant
     public string Name { get; set; } = string.Empty; //Tên của sản phẩm variant
+}
+
+
+public class AddItemToCartDTO
+{
+    public int ProductVariantId { get; set; } //Id của sản phẩm variant
+    public int Quantity { get; set; } //Số lượng sản phẩm variant trong giỏ hàng
 }
